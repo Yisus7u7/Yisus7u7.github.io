83a78e39964fc8deecfa0ddf4a6241bef78b1bc2
Yisus7u7 <jesuspixel5@gmail.com>
2021-04-20 19:34:54 -0500

    now

---
+++ mirrors/debs/packages-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from colorama import Fore, init, Back, Style
+import sys
+import os
+
+print(Fore.MAGENTA + "yisus/stable" + Fore.WHITE + Style.BRIGHT + "/" + Fore.GREEN + "candy-icon-theme")
+print(Fore.MAGENTA + "yisus/stable" + Fore.WHITE + Style.BRIGHT + "/" + Fore.GREEN + "tela-yellow-icon-theme")
+print(Fore.MAGENTA + "yisus/stable" + Fore.WHITE + Style.BRIGHT + "/" + Fore.GREEN + "oreo-blue-cursors-theme")
+print(Fore.MAGENTA + "yisus/stable" + Fore.WHITE + Style.BRIGHT + "/" + Fore.GREEN + "mac-themes")
+    

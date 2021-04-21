ff3bd5d59565b8c557783a9533abcbbe212c19fe
Yisus7u7 <jesuspixel5@gmail.com>
2021-04-20 19:51:09 -0500

    now

--- mirrors/debs/packages-list.py
+++ mirrors/debs/packages-list.py
@@ -5,8 +5,9 @@ from colorama import Fore, init, Back, Style
 import sys
 import os
 
-print(Fore.MAGENTA + "yisus/stable" + Fore.WHITE + Style.BRIGHT + "/" + Fore.GREEN + "candy-icon-theme")
-print(Fore.MAGENTA + "yisus/stable" + Fore.WHITE + Style.BRIGHT + "/" + Fore.GREEN + "tela-yellow-icon-theme")
-print(Fore.MAGENTA + "yisus/stable" + Fore.WHITE + Style.BRIGHT + "/" + Fore.GREEN + "oreo-blue-cursors-theme")
-print(Fore.MAGENTA + "yisus/stable" + Fore.WHITE + Style.BRIGHT + "/" + Fore.GREEN + "mac-themes")
-    
+print(Fore.MAGENTA + "yisus/stable/themes/x11" + Fore.WHITE + Style.BRIGHT + "/" + Fore.GREEN + "candy-icon-theme")
+print(Fore.MAGENTA + "yisus/stable/themes/x11" + Fore.WHITE + Style.BRIGHT + "/" + Fore.GREEN + "tela-yellow-icon-theme")
+print(Fore.MAGENTA + "yisus/stable/themes/x11" + Fore.WHITE + Style.BRIGHT + "/" + Fore.GREEN + "oreo-blue-cursors-theme")
+print(Fore.MAGENTA + "yisus/stable/themes/x11" + Fore.WHITE + Style.BRIGHT + "/" + Fore.GREEN + "mac-themes")
+print(Fore.MAGENTA + "yisus/stable/tools/x11" + Fore.WHITE + Style.BRIGHT + "/" + Fore.RED + "rapidpayload")
+print(Fore.YELLOW + "yisus/testing/tools" + Fore.WHITE + Style.BRIGHT + "/" + Fore.RED + "apktool")
